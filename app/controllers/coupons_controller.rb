5ba8b57a93ac18c1b905ec0ce45aa03482505848
devWRM <mcwen@comcast.net>
2020-08-21 18:41:20 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,26 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupons = Coupon.all
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+
+  end
+  
+  def create
+
+    ccoupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+
+
+    redirect_to coupon_path(ccoupon.id)
+  end
+
+
+
+
+end
